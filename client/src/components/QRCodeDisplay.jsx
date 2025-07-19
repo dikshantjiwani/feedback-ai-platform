56369c138baf5ba56ffb1253f4c0335a51c418ce
Dikshant Jiwani <dikshant.jiwani@gmail.com>
2025-07-19 11:23:10 +0530

    Updated client QR Code fix

--- client/src/components/QRCodeDisplay.jsx
+++ client/src/components/QRCodeDisplay.jsx
@@ -1,10 +1,18 @@
-import QRCode from "qrcode.react";
+import QRCode from "react-qrcode";
 
 export default function QRCodeDisplay({ url }) {
   return (
     <div className="flex flex-col items-center mt-2">
       <QRCode value={url} size={128} />
-      <a href={url} target="_blank" className="text-blue-600 underline mt-2">Open Link</a>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-600 underline mt-2"
+      >
+        Open Link
+      </a>
     </div>
   );
 }
+
